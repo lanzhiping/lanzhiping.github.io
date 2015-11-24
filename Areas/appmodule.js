1f745102dbbfc6458074549a84cf0129e0595062
lanzhiping <lannano@qq.com>
2015-11-24 22:56:45 +0800

    first commit

---
+++ Areas/appmodule.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function(){
+
+	angular.module('app',['ngMaterial'])
+			.controller('appCtrl',['$scope', function($scope){
+				$scope.text = 'this is my learning website.'
+			}])	;
+})();
