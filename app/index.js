f2ab9962cfb73349231e27ada1dbc696dc2e4a0e
Zhiping Lan <zhiping.lan@ge.com>
2018-11-06 10:37:10 +0800

    temp

--- app/index.js
+++ app/index.js
@@ -41,12 +41,12 @@ new Frameworks('.frameworks')
 
 
 function startApp() {
-    const root = window.document.querySelector('#app');
-    hyperhtml(root)`
-        <video class="videoplayer" id="video" width="100%" autoplay loop controlsList="nodownload nofullscreen noremoteplayback">
-            <source type="video/webm" src="https://download.videvo.net/videvo_files/video/free/2018-08/small_watermarked/180825_01_laptop_preview.webm">
-            <source type="video/mp4" src="https://download.videvo.net/videvo_files/video/free/2018-08/small_watermarked/180825_01_laptop_preview.mp4">
-        </video>
-    `
+    // const root = window.document.querySelector('#app');
+    // hyperhtml(root)`
+    //     <video class="videoplayer" id="video" width="100%" autoplay loop controlsList="nodownload nofullscreen noremoteplayback">
+    //         <source type="video/webm" src="https://download.videvo.net/videvo_files/video/free/2018-08/small_watermarked/180825_01_laptop_preview.webm">
+    //         <source type="video/mp4" src="https://download.videvo.net/videvo_files/video/free/2018-08/small_watermarked/180825_01_laptop_preview.mp4">
+    //     </video>
+    // `
 }
 // startApp();
